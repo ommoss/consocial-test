e429e90e6a21f8a19059687886b160d6be7c2afd
Owen Moss <ommoss63@gmail.com>
2016-11-21 20:35:15 +0000

    merging fixeds

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -26,7 +26,7 @@ class Main extends React.Component {
           </Col>
             <Col xs={8} md={8}>
           <div>
-            <GoogleMap />
+            {this.props.data.one}
           </div>
             </Col>
         </Row>
