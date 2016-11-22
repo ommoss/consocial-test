3d9a94edd298f559f827cf3eb37c845f85a30489
Owen Moss <ommoss63@gmail.com>
2016-11-22 23:16:19 +0000

    fixed knex error in migration

--- migrations/20161122220037_initailBanana.js
+++ migrations/20161122220037_initailBanana.js
@@ -36,8 +36,8 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
      return Promise.all([
         knex.schema.dropTable('users'),
-        knex.schema.dropTable('games')
-        knex.schema.dropTable('tournament'),
+        knex.schema.dropTable('games'),
+        knex.schema.dropTable('tournament')
     ])
 
 };
