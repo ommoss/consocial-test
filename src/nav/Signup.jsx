c2bda303f22ac5450d5e8853b10700cdb25938f5
Todd Ferguson <toddsferg@gmail.com>
2016-11-23 22:03:13 +0000

    add sign up initial sign up modal functionality

--- src/nav/Signup.jsx
+++ src/nav/Signup.jsx
@@ -9,8 +9,53 @@ import Button from 'react-bootstrap/lib/Button';
  class Signup extends React.Component {
   constructor(props){
     super(props);
-    this.state = {}
+     this.formCheck = this.formCheck.bind(this);
+    // this.loginCheck = this.loginCheck.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.close = this.close.bind(this);
+    this.state = {
+      username: "",
+      password: "",
+      firstname: "",
+      lastname: "",
+      email: "",
+      u1: "bob",
+      p1: "bob"
+    }
   }
+
+ //    handleSubmit(event){
+ //      if(event.key === "Enter"){
+ //        this.loginCheck();
+ //        console.log('register handleSubnmit')
+ //      }
+
+ // }
+
+
+  handleChange(event){
+    if(event.target.id === "userName"){
+      this.setState({ username: event.target.value})
+      console.log(this.state.username);
+    } else if (event.target.id ==="firstName"){
+      this.setState({firstname: event.target.value});
+    } else if (event.target.id ==="email"){
+      this.setState({email: event.target.value});
+    } else if (event.target.id ==="lastName"){
+      this.setState({lastname: event.target.value});
+    } else if (event.target.id ==="password"){
+      this.setState({password: event.target.value});
+    }
+  }
+
+  formCheck(){
+    if(this.state.username && this.state.firstname && this.state.lastname && this.state.email && this.state.password){
+
+    }else {alert("please fill in all forms")}
+  }
+
+
 
   render() {
     let close = () => this.setState({ show: false});
@@ -28,21 +73,25 @@ import Button from 'react-bootstrap/lib/Button';
           </Modal.Header>
           <Modal.Body>
             <form id = "signup">
-              <FormGroup controlId = "signup" >
+              <FormGroup >
                 <ControlLabel>Username</ControlLabel>
-                <FormControl type = "text" />
+                <FormControl id ="userName" type = "text" value={this.state.username} onChange={this.handleChange}/>
+                <ControlLabel>First Name</ControlLabel>
+                <FormControl type = "text" id="firstName" value={this.state.firstname} onChange={this.handleChange} />
+                <ControlLabel>Last Name</ControlLabel>
+                <FormControl type = "text" id="lastName" value={this.state.lastname} onChange={this.handleChange}/>
                 <ControlLabel>Email</ControlLabel>
-                <FormControl type = "text" />
+                <FormControl type = "text" id="email" value={this.state.email} onChange={this.handleChange}/>
                 <ControlLabel>Password</ControlLabel>
-                <FormControl type = "password" />
+                <FormControl type = "password" id="password" value={this.state.password} onChange={this.handleChange}/>
                 <ControlLabel>Password Confirm</ControlLabel>
-                <FormControl type = "password" />
+                <FormControl type = "password" id="password" value={this.state.password} onChange={this.handleChange}/>
 
               </FormGroup>
             </form>
           </Modal.Body>
           <Modal.Footer>
-            <Button>Submit</Button>
+            <Button onClick={this.formCheck}>Submit</Button>
             <Button onClick={close}>Close</Button>
           </Modal.Footer>
         </Modal>
